873a0f71d0d94ef7b52c06e6ac1b84ee9870c212
Deval Hole <devalhole.pict@gmail.com>
2026-04-12 00:36:04 +0530

    fix: convert s-backend from submodule to normal folder

---
+++ s-backend/src/routes/auth.routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import passport from "passport";
+import { googleCallback, login, register } from "../controllers/auth.controllers.js";
+import { validateLoginUser, validateRegisterUser } from "../validators/app.validators.js";
+
+const router = express.Router();
+
+router.post("/register", validateRegisterUser, register);
+router.post("/login", validateLoginUser, login);
+
+router.get(
+    "/google",
+    passport.authenticate("google", { scope: [ "profile", "email" ] })
+);
+
+router.get(
+    "/google/callback",
+    passport.authenticate("google", {
+        session: false,
+        failureRedirect: "http://localhost:5173/login"
+    }),
+    googleCallback
+);
+
+export default router;
