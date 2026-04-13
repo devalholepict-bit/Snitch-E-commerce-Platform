7dcd1a9522dbf0d357bc9cb1a85895c047add347
Deval Hole <devalhole.pict@gmail.com>
2026-04-13 17:00:24 +0530

    feat: refactor main.jsx to remove StrictMode and improve rendering structure

--- s-frontend/src/main.jsx
+++ s-frontend/src/main.jsx
@@ -1,14 +1,30 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { Provider } from 'react-redux'
+// import './index.css'
+// import App from './app/App.jsx'
+// import { store } from './app/app.store.js'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </StrictMode>,
+// )
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import './index.css'
 import App from './app/App.jsx'
+import { Provider } from 'react-redux'
 import { store } from './app/app.store.js'
 
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
+
+  <Provider store={store} >
+    <App />
+  </Provider>
+
 )
