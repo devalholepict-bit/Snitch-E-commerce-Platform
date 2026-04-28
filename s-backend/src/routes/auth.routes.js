ade6cbad7b8c56707da3639cedb25104005611fd
Deval Hole <devalhole.pict@gmail.com>
2026-04-28 17:28:32 +0530

    feat: Implement cart management features including add, get, and update cart items, along with payment processing

--- s-backend/src/routes/auth.routes.js
+++ s-backend/src/routes/auth.routes.js
@@ -1,27 +1,37 @@
-import express from "express";
+import { Router } from "express";
+import { validateRegisterUser, validateLoginUser } from "../validator/auth.validator.js";
+import { getMe, googleCallback, login, register } from "../controllers/auth.controller.js";
 import passport from "passport";
-import { googleCallback, login, register } from "../controllers/auth.controllers.js";
-import { validateLoginUser, validateRegisterUser } from "../validators/app.validators.js";
+import { config } from "../config/config.js";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
 
-const router = express.Router();
+const router = Router();
 
-router.post("/register", validateRegisterUser, register);
-router.post("/login", validateLoginUser, login);
 
-router.get(
-    "/google",
-    passport.authenticate("google", { scope: [ "profile", "email" ] })
-);
 
-router.get(
-    "/google/callback",
+router.post('/register', validateRegisterUser, register)
+
+router.post("/login", validateLoginUser, login)
+
+
+// /api/auth/google
+router.get("/google",
+    passport.authenticate("google", { scope: [ "profile", "email" ] }))
+
+router.get("/google/callback",
     passport.authenticate("google", {
         session: false,
-        failureRedirect: "http://localhost:5173/login"
+        failureRedirect: config.NODE_ENV == "development" ? "http://localhost:5173/login" : "/login"
     }),
-    googleCallback
-);
+    googleCallback,
+)
 
-router.get('/me', authenticateUser, getMe);
+
+/**
+ * @route GET /api/auth/me
+ * @description Get the authenticated user's profile
+ * @access Private
+ */
+router.get('/me', authenticateUser, getMe)
 
 export default router;
